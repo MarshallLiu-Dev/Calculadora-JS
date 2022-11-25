1a0b0bce1753ccbe22e9d2cdba68b8ca39c55eed
MarshallLiu-Dev <paulomartins12358@gmail.com>
2022-11-25 16:20:58 -0300

    frist commit master

--- scripts/script.js
+++ scripts/script.js
@@ -13,10 +13,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
   /* EVENTOS DOS BOTÕES */
   buttons.forEach((btn) => {
+
     //receber os valores dos botões
     btn.addEventListener("click", (e) => {
       const value = e.target.innerText;
       console.log(value);
+
+// numeros 
+      if (+value >= 0 || value === "."){
+        calc.addDigit(value);
+      }else{
+        calc.processOperation(value);
+      }
+
     });
   });
 
@@ -28,5 +37,53 @@ document.addEventListener("DOMContentLoaded", function () {
 
       this.currentOperation = "";
     }
+  // mostrando os digitos no visor
+  addDigit(digit){
+// Checando se a operação ja tem ponto
+    if(digit === "." && this.currentOperationText.innerText.includes(".")){
+      return;
+    };
+
+   this.currentOperation = digit
+   this.updateScreen()
+  };
+// Processos de operação na calculadora
+  processOperation(operation){
+
+//obter valor atual e anterior
+ let operationValue;
+ const previous = +this.previousOperationText.innerText;
+ const current = +this.currentOperationText.innerText;
+
+// switch() verifica a operação
+ switch(operation){
+  case "+":
+    operationValue = previous + current;
+    this.updateScreen(operationValue, operation, current, previous);
+    break;
+    default:
+      return;
+ }
+
+}
+// mudando valores na tela da caulculadora 
+updateScreen(operationValue = null, operation = null, previous = null){ 
+
+  console.log(operationValue, operation, current, previous);
+   
+  if(operationValue === null){
+      this.currentOperationText.innerText += this.currentOperation;
+  }else{
+//verifique se o valor é zero, se apenas adicionar o valor atual
+    if (previous === 0) {
+      operationValue = current;
+    }
+//adicionar valor atual ao anterior
+    this.previousOperationText.innerText = `${operationValue} ${operation}`;
+    this.currentOperationText.innerText = "";
+    }
   }
+}
 });
+
+const calc = new Calculator(previousOperationText, currentOperationText);
