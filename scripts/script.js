0e38f34f7960b6dd915cbdaf87158fd83d367c85
MarshallLiu-Dev <paulomartins12358@gmail.com>
2022-11-24 22:05:34 -0300

    third commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,32 @@
-//Operação anterior
-const previousOperationText = document.querySelector("#previous-operation");
+//O DOMContentLoaded é um evento que dispara quando o conteúdo DOM é carregado,
+//sem esperar que as imagens e folhas de estilo terminem de carregar.
+document.addEventListener("DOMContentLoaded", function () {
+  //Operação anterior
+  const previousOperationText = document.querySelector("#previous-operation");
 
-//Operação atual
-const currentOperationText = document.querySelector("#current-operation");
+  //Operação atual
+  const currentOperationText = document.querySelector("#current-operation");
 
-//Botoes
-const buttons = document.querySelectorAll("#buttons-container button");
-console.log(buttons);
+  //Botoes
+  const buttons = document.querySelectorAll("#buttons-container button");
+  console.log(buttons);
 
-/* EVENTOS DOS BOTÕES */
-buttons.forEach((btn) => {
-  //receber os valores dos botões
-  btn.addEventListener("click", (e) => {
-    const value = e.target.innerText;
-    console.log(value);
+  /* EVENTOS DOS BOTÕES */
+  buttons.forEach((btn) => {
+    //receber os valores dos botões
+    btn.addEventListener("click", (e) => {
+      const value = e.target.innerText;
+      console.log(value);
+    });
   });
-});
 
-/* LOGICA DA APLICAÇÃO*/
-class Calculator {
-  constructor(previousOperationText, currentOperationText) {
-    this.previousOperationText = previousOperationText;
-    this.currentOperationText = currentOperationText;
+  /* LOGICA DA APLICAÇÃO*/
+  class Calculator {
+    constructor(previousOperationText, currentOperationText) {
+      this.previousOperationText = previousOperationText;
+      this.currentOperationText = currentOperationText;
 
-    this.currentOperation = "";
+      this.currentOperation = "";
+    }
   }
-}
+});
